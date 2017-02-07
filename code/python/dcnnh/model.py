d924ef708ac74b131ed7f44196806bd567a247d8
haoyuanchi <hychi@qq.com>
2017-02-07 18:17:37 +0100

    dbnh

---
+++ code/python/dcnnh/model.py
@@ -0,0 +1,224 @@
+import binary_net
+from lasagne.layers import InputLayer, Conv2DLayer
+from lasagne.layers import MaxPool2DLayer, LocalResponseNormalization2DLayer
+from lasagne.layers import SliceLayer, concat, DenseLayer
+import lasagne
+
+
+activation = binary_net.binary_tanh_unit
+
+
+def bulid_model_vgg_like(input_var, hash_bit, which_data):
+    network = {}
+    if which_data=='mnist':
+        network['input'] = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+                                                     input_var=input_var)
+
+    elif which_data=='svhn':
+        network['input'] = lasagne.layers.InputLayer(shape=(None, 3, 32, 32),
+                                                     input_var=input_var)
+
+    elif which_data=='cifar10':
+        network['input'] = lasagne.layers.InputLayer(shape=(None, 3, 32, 32),
+                                                     input_var=input_var)
+
+    elif which_data=='cifar100':
+        network['input'] = lasagne.layers.InputLayer(shape=(None, 3, 32, 32),
+                                                     input_var=input_var)
+
+    # 128C3-128C3-P2
+    network['conv1_1'] = binary_net.Conv2DLayer(network['input'],
+                                                num_filters=128,
+                                                filter_size=(3, 3),
+                                                pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    network['bn1_1'] = lasagne.layers.BatchNormLayer(network['conv1_1'])
+    network['ac1_1'] = lasagne.layers.NonlinearityLayer(network['bn1_1'], nonlinearity=activation)
+
+    network['conv1_2'] = binary_net.Conv2DLayer(network['ac1_1'],
+                                              num_filters=128,
+                                              filter_size=(3, 3),
+                                              pad=1,
+                                              nonlinearity=lasagne.nonlinearities.identity)
+    network['pool1'] = lasagne.layers.MaxPool2DLayer(network['conv1_2'], pool_size=(2, 2))
+    network['bn1_2'] = lasagne.layers.BatchNormLayer(network['pool1'])
+    network['ac1_2'] = lasagne.layers.NonlinearityLayer(network['bn1_2'], nonlinearity=activation)
+
+    # 256C3-256C3-P2
+    network['conv2_1'] = binary_net.Conv2DLayer(network['ac1_2'],
+                                                num_filters=256,
+                                                filter_size=(3, 3),
+                                                pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    network['bn2_1'] = lasagne.layers.BatchNormLayer(network['conv2_1'])
+    network['ac2_1'] = lasagne.layers.NonlinearityLayer(network['bn2_1'], nonlinearity=activation)
+
+    network['conv2_2'] = binary_net.Conv2DLayer(network['ac2_1'],
+                                                num_filters=256,
+                                                filter_size=(3, 3),
+                                                pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    network['pool2'] = lasagne.layers.MaxPool2DLayer(network['conv2_2'], pool_size=(2, 2))
+    network['bn2_2'] = lasagne.layers.BatchNormLayer(network['pool2'])
+    network['ac2_2'] = lasagne.layers.NonlinearityLayer(network['bn2_2'], nonlinearity=activation)
+
+    # 512C3-512C3-P2
+    network['conv3_1'] = binary_net.Conv2DLayer(network['ac2_2'],
+                                                num_filters=512,
+                                                filter_size=(3, 3),
+                                                pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    network['bn3_1'] = lasagne.layers.BatchNormLayer(network['conv3_1'])
+    network['ac3_1'] = lasagne.layers.NonlinearityLayer(network['bn3_1'], nonlinearity=activation)
+
+    network['conv3_2'] = binary_net.Conv2DLayer(network['ac3_1'],
+                                                num_filters=512,
+                                                filter_size=(3, 3),
+                                                pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    network['pool3'] = lasagne.layers.MaxPool2DLayer(network['conv3_2'], pool_size=(2, 2))
+    network['bn3_2'] = lasagne.layers.BatchNormLayer(network['pool3'])
+    network['ac3_2'] = lasagne.layers.NonlinearityLayer(network['bn3_2'], nonlinearity=activation)
+
+    # 1024FP-1024FP-10FP
+    network['fc4'] = binary_net.DenseLayer(network['ac3_2'], num_units=1024, nonlinearity=lasagne.nonlinearities.identity)
+    network['bn4'] = lasagne.layers.BatchNormLayer(network['fc4'])
+    network['ac4'] = lasagne.layers.NonlinearityLayer(network['bn4'], nonlinearity=activation)
+
+    network['fc_hash'] = binary_net.DenseLayer(network['ac4'], num_units=hash_bit, nonlinearity=lasagne.nonlinearities.identity)
+    network['bn5'] = lasagne.layers.BatchNormLayer(network['fc_hash'])
+    network['hash_out'] = lasagne.layers.NonlinearityLayer(network['bn5'], nonlinearity=activation)
+
+    network['fc6'] = binary_net.DenseLayer(network['hash_out'], num_units=10, nonlinearity=lasagne.nonlinearities.identity)
+    network['bn6'] = lasagne.layers.BatchNormLayer(network['fc6'])
+    network['prob'] = lasagne.layers.NonlinearityLayer(network['bn6'], nonlinearity=lasagne.nonlinearities.softmax)
+
+    return network
+
+
+
+
+def build_model_alexnet(input_var, hash_bit, which_data):
+    net = {}
+
+    net['input'] = InputLayer((None, 3, 227, 227))
+
+    # conv1
+    net['conv1'] = binary_net.Conv2DLayer(
+        net['data'],
+        num_filters=96,
+        filter_size=(11, 11),
+        stride=4,
+        nonlinearity=lasagne.nonlinearities.identity)
+
+    # pool1
+    net['pool1'] = MaxPool2DLayer(net['conv1'], pool_size=(3, 3), stride=2)
+
+    net['bn1'] = lasagne.layers.BatchNormLayer(net['conv1'])
+    net['ac1'] = lasagne.layers.NonlinearityLayer(net['bn1'], nonlinearity=activation)
+
+    # conv2
+    net['conv2_data1'] = SliceLayer(net['ac1'], indices=slice(0, 48), axis=1)
+    net['conv2_data2'] = SliceLayer(net['ac1'], indices=slice(48, 96), axis=1)
+
+    # now do the convolutions
+    net['conv2_part1'] = binary_net.Conv2DLayer(net['conv2_data1'],
+                                     num_filters=128,
+                                     filter_size=(5, 5),
+                                     pad=2,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    net['conv2_part2'] = binary_net.Conv2DLayer(net['conv2_data2'],
+                                     num_filters=128,
+                                     filter_size=(5, 5),
+                                     pad=2,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+
+    # now combine
+    net['conv2'] = concat((net['conv2_part1'], net['conv2_part2']), axis=1)
+
+    # pool2
+    net['pool2'] = MaxPool2DLayer(net['conv2'], pool_size=(3, 3), stride=2)
+
+    # norm2
+    net['bn2'] = lasagne.layers.BatchNormLayer(net['pool2'])
+    net['ac2'] = lasagne.layers.NonlinearityLayer(net['bn2'], nonlinearity=activation)
+
+    # conv3
+    # no group
+    net['conv3'] = binary_net.Conv2DLayer(net['ac2'],
+                               num_filters=384,
+                               filter_size=(3, 3),
+                               pad=1,
+                                          nonlinearity=lasagne.nonlinearities.identity)
+    net['ac3'] = lasagne.layers.NonlinearityLayer(net['conv3'], nonlinearity=activation)
+
+
+    # conv4
+    # group = 2
+    net['conv4_data1'] = SliceLayer(net['ac3'], indices=slice(0, 192), axis=1)
+    net['conv4_data2'] = SliceLayer(net['ac3'], indices=slice(192, 384), axis=1)
+    net['conv4_part1'] = binary_net.Conv2DLayer(net['conv4_data1'],
+                                     num_filters=192,
+                                     filter_size=(3, 3),
+                                     pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    net['conv4_part2'] = binary_net.Conv2DLayer(net['conv4_data2'],
+                                     num_filters=192,
+                                     filter_size=(3, 3),
+                                     pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    net['conv4'] = concat((net['conv4_part1'], net['conv4_part2']), axis=1)
+    net['ac4'] = lasagne.layers.NonlinearityLayer(net['conv4'], nonlinearity=activation)
+
+
+    # conv5
+    # group 2
+    net['conv5_data1'] = SliceLayer(net['ac4'], indices=slice(0, 192), axis=1)
+    net['conv5_data2'] = SliceLayer(net['ac4'], indices=slice(192, 384), axis=1)
+    net['conv5_part1'] = binary_net.Conv2DLayer(net['conv5_data1'],
+                                     num_filters=128,
+                                     filter_size=(3, 3),
+                                     pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    net['conv5_part2'] = binary_net.Conv2DLayer(net['conv5_data2'],
+                                     num_filters=128,
+                                     filter_size=(3, 3),
+                                     pad=1,
+                                                nonlinearity=lasagne.nonlinearities.identity)
+    net['conv5'] = concat((net['conv5_part1'], net['conv5_part2']), axis=1)
+
+    # pool 5
+    net['pool5'] = MaxPool2DLayer(net['conv5'], pool_size=(3, 3), stride=2)
+
+    net['ac5'] = lasagne.layers.NonlinearityLayer(net['pool5'], nonlinearity=activation)
+
+    # fc6
+    net['fc6'] = binary_net.DenseLayer(
+        net['ac5'], num_units=4096,
+        nonlinearity=lasagne.nonlinearities.identity)
+    net['bn6'] = lasagne.layers.BatchNormLayer(net['fc6'])
+
+    net['ac6'] = lasagne.layers.NonlinearityLayer(net['bn6'], nonlinearity=activation)
+
+    # fc7
+    net['fc7'] = binary_net.DenseLayer(
+        net['ac6'],
+        num_units=4096,
+        nonlinearity=lasagne.nonlinearities.identity)
+    net['bn7'] = lasagne.layers.BatchNormLayer(net['fc7'])
+    net['ac7'] = lasagne.layers.NonlinearityLayer(net['bn7'], nonlinearity=activation)
+
+    net['hash'] = binary_net.DenseLayer(
+        net['ac7'],
+        num_units=hash_bit,
+        nonlinearity=lasagne.nonlinearities.identity)
+    net['bn8'] = lasagne.layers.BatchNormLayer(net['hash'])
+    net['ac8'] = lasagne.layers.NonlinearityLayer(net['bn8'], nonlinearity=activation)
+
+    # fc8
+    net['fc8'] = binary_net.DenseLayer(
+        net['ac8'],
+        num_units=10,
+        nonlinearity=lasagne.nonlinearities.softmax)
+
+    return net
--- code/python/dcnnh/train_hash_temp.py
+++ code/python/dcnnh/train_hash_temp.py
@@ -18,8 +18,12 @@ from collections import OrderedDict
 import time
 from scipy.misc import imresize
 
+
 feature_width = 256
 feature_height = 256
+batch_size = 100
+num_epochs = 500
+hash_bits = 64
 
 
 def cropImage(im):
@@ -60,28 +64,6 @@ def load_dataset(which_data='cifar10', outputlayer='Logistic'):
     return datasets
 
 
-# A function which shuffles a dataset
-def shuffle(X, y):
-    shuffle_parts = 1
-    chunk_size = len(X) / shuffle_parts
-    shuffled_range = range(chunk_size)
-
-    X_buffer = np.copy(X[0:chunk_size])
-    y_buffer = np.copy(y[0:chunk_size])
-
-    for k in range(shuffle_parts):
-        np.random.shuffle(shuffled_range)
-
-        for i in range(chunk_size):
-            X_buffer[i] = X[k * chunk_size + shuffled_range[i]]
-            y_buffer[i] = y[k * chunk_size + shuffled_range[i]]
-
-        X[k * chunk_size:(k + 1) * chunk_size] = X_buffer
-        y[k * chunk_size:(k + 1) * chunk_size] = y_buffer
-
-    return X, y
-
-
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     assert len(inputs) == len(targets)
 
@@ -96,68 +78,62 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
         yield inputs[excerpt], targets[excerpt]
 
 
-def main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_set_y, which_data,
-         batch_size, num_epochs, hash_bit,
-         save_model_path, save_hash_path):
+def main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_set_y, which_data, save_path):
 
     # Prepare Theano variables for inputs and targets
     input_var = T.tensor4('inputs')
-    target_var = T.matrix('targets')
+    target_var = T.ivector('targets')
     LR = T.scalar('LR', dtype=theano.config.floatX)
 
     LR_start = 0.01
     LR_fin = 0.0000003
     LR_decay = (LR_fin / LR_start) ** (1. / num_epochs)
 
-    network = model.bulid_model_vgg_like(input_var, hash_bit=hash_bit, which_data=which_data)
+    network = model.bulid_model_vgg_like(input_var, hash_bit=hash_bits, which_data=which_data)
 
-    #############  train
-    output_layer = network['prob']
-    hash_layer = network['hash_out']
+    train_output = lasagne.layers.get_output(network, deterministic=False)
+
+    output_layer = network[-1]
+    hash_layer = network['hash']
     hash_out = lasagne.layers.get_output(hash_layer)
 
-    prediction = lasagne.layers.get_output(output_layer, deterministic=False)
+    prediction = lasagne.layers.get_output(output_layer)
 
-    ##########    lose function cross entropy
-    # loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-    # loss = loss.mean()
-    ##########    lose function hinge
-    loss = T.mean(T.sqr(T.maximum(0., 1. - prediction * target_var)))
+    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+    loss = loss.mean()
 
-    # # making each bit has 50% probability of being one or zero
-    # # sum the batch size
+    # making each bit has 50% probability of being one or zero
+    # sum the batch size
     avg = theano.tensor.sum(hash_out, 0) / batch_size
-    even_dist_loss = lasagne.regularization.l2(avg)
-    # even_dist_loss = lasagne.regularization.l1(avg)
-    even_dist_loss_weight = 0.2
+    even_dist_loss = lasagne.regularization.l2(avg - 0.5)
+    even_dist_loss_weight = 0.1
 
     loss = loss + even_dist_loss * even_dist_loss_weight
 
     ############ update
     # W updates
-    W = lasagne.layers.get_all_params(output_layer, binary=True)
-    W_grads = binary_net.compute_grads(loss, output_layer)
+    W = lasagne.layers.get_all_params(network, binary=True)
+    W_grads = binary_net.compute_grads(loss, network)
     updates = lasagne.updates.adam(loss_or_grads=W_grads, params=W, learning_rate=LR)
-    updates = binary_net.clipping_scaling(updates, output_layer)
+    updates = binary_net.clipping_scaling(updates, network)
 
     # other parameters updates
-    params = lasagne.layers.get_all_params(output_layer, trainable=True, binary=False)
+    params = lasagne.layers.get_all_params(network, trainable=True, binary=False)
     updates = OrderedDict(
         updates.items() + lasagne.updates.adam(loss_or_grads=loss, params=params, learning_rate=LR).items())
 
-    ############# test
-    ##########    lose function cross entropy
-    # test_prediction = lasagne.layers.get_output(output_layer, deterministic=True)
-    # test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
-    #                                                         target_var)
-    # test_loss = test_loss.mean()
-    # test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var), dtype=theano.config.floatX)
-
-    ##########    lose function hinge
+    ############### test
     test_prediction = lasagne.layers.get_output(output_layer, deterministic=True)
-    test_loss = T.mean(T.sqr(T.maximum(0., 1. - prediction * target_var)))
+    test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
+                                                            target_var)
+    test_loss = test_loss.mean()
     # As a bonus, also create an expression for the classification accuracy:
-    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), T.argmax(target_var, axis=1)), dtype=theano.config.floatX)
+    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                      dtype=theano.config.floatX)
+
+    # test_output = lasagne.layers.get_output(network, deterministic=True)
+    # test_loss = T.mean(T.sqr(T.maximum(0., 1. - target_var * test_output)))
+    # test_acc = T.mean(T.neq(T.argmax(test_output, axis=1), T.argmax(target_var, axis=1)), dtype=theano.config.floatX)
 
     train_fn = theano.function([input_var, target_var, LR], loss, updates=updates)
     val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
@@ -173,11 +149,13 @@ def main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_se
     def train_epoch(X, y, LR):
         # In each epoch, we do a full pass over the training data:
         train_err = 0
-        batches = len(X) / batch_size
-        for i in range(batches):
-            train_err += train_fn(X[i * batch_size:(i + 1) * batch_size], y[i * batch_size:(i + 1) * batch_size], LR)
-
+        batches = 0
+        for batch in iterate_minibatches(X, y, batch_size, shuffle=True):
+            inputs, targets = batch
+            train_err += train_fn(inputs, targets, LR)
+            batches += 1
         train_err /= batches
+
         return train_err
 
     # This function tests the model a full epoch (on the whole dataset)
@@ -185,12 +163,13 @@ def main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_se
         # And a full pass over the validation data:
         val_err = 0
         val_acc = 0
-        batches = len(X) / batch_size
-        for i in range(batches):
-            err, acc = val_fn(X[i * batch_size:(i + 1) * batch_size], y[i * batch_size:(i + 1) * batch_size])
+        batches = 0
+        for batch in iterate_minibatches(X, y, batch_size, shuffle=False):
+            inputs, targets = batch
+            err, acc = val_fn(inputs, targets)
             val_err += err
             val_acc += acc
-
+            batches += 1
         val_acc = val_acc / batches * 100
         val_err /= batches
 
@@ -200,23 +179,19 @@ def main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_se
     for epoch in range(num_epochs):
         start_time = time.time()
 
-        train_set_x, train_set_y = shuffle(train_set_x, train_set_y)
-
-        train_err = train_epoch(train_set_x, train_set_y, LR)
+        train_err = train_epoch(X_train, train_set_y, LR)
         val_err, val_acc = val_epoch(valid_set_x, valid_set_y)
 
         # test if validation error went down
         if val_acc <= best_val_acc:
 
-            best_val_acc = val_acc
+            best_val_err = val_err
             best_epoch = epoch + 1
 
-            test_loss, test_acc = val_epoch(test_set_x, test_set_y)
+            test_err, test_loss = val_epoch(test_set_x, test_set_y)
 
-            if save_model_path is not None:
-                np.savez(save_model_path, *lasagne.layers.get_all_param_values(output_layer))
-            if save_hash_path is not None:
-                np.savez(save_hash_path, *lasagne.layers.get_all_param_values(hash_layer))
+            if save_path is not None:
+                np.savez(save_path, *lasagne.layers.get_all_param_values(network))
 
         epoch_duration = time.time() - start_time
 
@@ -229,7 +204,7 @@ def main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_se
         print("  best epoch:                    " + str(best_epoch))
         print("  best validation error rate:    " + str(best_val_acc) + "%")
         print("  test loss:                     " + str(test_loss))
-        print("  test error rate:               " + str(test_acc) + "%")
+        print("  test error rate:               " + str(test_err) + "%")
 
         # decay the LR
         LR *= LR_decay
@@ -237,9 +212,11 @@ def main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_se
     return network
 
 
-if __name__ == "__main__":
-    which_data = 'cifar10'
 
+
+if __name__ == "__main__":
+    which_data = 'mnist'
+    save_path = 'binary_net_cifar10_hash.npz'
 
     # Load the dataset
     print("Loading data...")
@@ -248,41 +225,43 @@ if __name__ == "__main__":
     valid_set_x, valid_set_y = datasets[1]
     test_set_x, test_set_y = datasets[2]
 
-    train_set_x = np.subtract(np.multiply(2., train_set_x), 1.)
-    valid_set_x = np.subtract(np.multiply(2., valid_set_x), 1.)
-    test_set_x = np.subtract(np.multiply(2., test_set_x), 1.)
-
-    # flatten targets
-    train_set_y = np.hstack(train_set_y)
-    valid_set_y = np.hstack(valid_set_y)
-    test_set_y = np.hstack(test_set_y)
-
-    # Onehot the targets
-    train_set_y = np.float32(np.eye(10)[train_set_y])
-    valid_set_y = np.float32(np.eye(10)[valid_set_y])
-    test_set_y = np.float32(np.eye(10)[test_set_y])
-
-    # for hinge loss
-    train_set_y = 2 * train_set_y - 1.
-    valid_set_y = 2 * valid_set_y - 1.
-    test_set_y = 2 * test_set_y - 1.
-
     del datasets
 
-    batch_size = 100
-    num_epochs = 500
-    hash_bits = np.array([12, 24, 32, 48])
+    # X_train = np.zeros((50000, 1, 256, 256), dtype=np.float32)
+    # for i in range(train_set_x.shape[0]):
+    #     temp = image_preprocess.resize_image(train_set_x[i, :, :, :].transpose(1, 2, 0), (feature_width, feature_height))
+    #     X_train[i, :, :, :] = temp.transpose(2, 0, 1)
+    #
+    # X_train = image_preprocess.oversample(X_train, (227, 227))
+    # X_valid = np.zeros((valid_set_x.shape[0], 3, 256, 256), dtype=np.float32)
+    # X_test = np.zeros((test_set_x.shape[0], 3, 256, 256), dtype=np.float32)
+    #
+    # x_temp = image_preprocess.resize_image(test_set_x, (feature_width, feature_height))
+    # x_temp = image_preprocess.oversample(x_temp, (227,227))
+    #
+    # # crop features and resize the image
+    # X_train = np.asarray(map(cropImage, train_set_x))
+    # X_valid = np.asarray(map(cropImage, valid_set_x))
+    # X_test = np.asarray(map(cropImage, test_set_x))
+    #
+    # del train_set_x, valid_set_x, test_set_x
+    #
+    # # flatten targets
+    # train_set_y = np.hstack(train_set_y)
+    # valid_set_y = np.hstack(valid_set_y)
+    # test_set_y = np.hstack(test_set_y)
+    #
+    # # Onehot the targets
+    # train_set_y = np.float32(np.eye(10)[train_set_y])
+    # valid_set_y = np.float32(np.eye(10)[valid_set_y])
+    # test_set_y = np.float32(np.eye(10)[test_set_y])
+    #
+    # # for hinge loss
+    # train_set_y = 2 * train_set_y - 1.
+    # valid_set_y = 2 * valid_set_y - 1.
+    # test_set_y = 2 * test_set_y - 1.
 
-    for i in range(len(hash_bits)):
-        hash_bit = hash_bits[i]
-        #  'You have {} things.'.format(things)  # str.format()
-        #  'You have %d things.' % things  # % interpolation
-        #  f'You have {things} things.'  # f-string (since Python 3.6)
-        save_model_path = 'bdhn_cifar10_model_' + str(hash_bit) + '.npz'
-        save_hash_path = 'bdhn_cifar10_hash_' + str(hash_bit) + '.npz'
-        network = main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_set_y, which_data,
-                       batch_size, num_epochs, hash_bit,
-                       save_model_path, save_hash_path)
+    network = main(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_set_y, which_data, save_path)
 
 
 
